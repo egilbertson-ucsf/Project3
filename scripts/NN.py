897baa2ed8b5acfa8aa82c47d4f022af29664fc3
noamteyssier <nb.teyssier@gmail.com>
2021-03-11 16:49:42 -0800

    functional autoencoder

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,143 +1,218 @@
 #!/usr/bin/env python3
-
 import numpy as np
-
 
 class NeuralNetwork:
 
-    def __init__(self, layers, learning_rate=0.01):
-        self.layers_config = np.array(layers)
-        self.learning_rate = learning_rate
-        self.weights, self.bias = self.make_weights()
-        self.nodes = [np.zeros(x) for x in self.layers_config]
-        self.activations = [np.zeros_like(x) for x in self.nodes]
+    def __init__(self, layers = [3, 2, 3], learning_rate=0.1):
 
-    def make_weights(self):
+        self.layers = np.array(layers)
+        self.learning_rate = np.array(learning_rate).reshape(1, 1)
+
+        self.params = {
+            "weights": [],
+            "bias": [],
+            "zs": [],
+            "as": []
+        }
+
+        self.d_weights = []
+        self.d_bias = []
+
+        self.initialize_params()
+
+    def initialize_params(self):
         """
-        generates initial weights in list
+        randomly initializes weights and biases
 
-        each weight matrix (N_l x N_l+1) represented the edges between
-        all nodes in layer l to all nodes in layer L+1.
+        stores all internal parameters within indexable dictionary
         """
 
-        self.weights = []
-        self.bias = []
-        for i in np.arange(self.layers_config.size-1):
-            weights = np.random.random(
-                (self.layers_config[i], self.layers_config[i + 1])
+        for i in np.arange(self.layers.size):
+
+            if i > 0:
+                self.params['weights'].append(
+                    np.random.random(
+                        (self.layers[i], self.layers[i-1])
+                    )
                 )
-            bias = np.random.random(self.layers_config[i + 1])
-            self.weights.append(weights)
-            self.bias.append(bias)
 
-        return self.weights, self.bias
+                self.params['bias'].append(
+                    np.random.random(self.layers[i])
+                )
 
-    def Sigmoid(self, x):
+
+
+            self.params['zs'].append(
+                np.zeros(self.layers[i]).\
+                    reshape(self.layers[i], 1)
+            )
+
+            self.params['as'].append(
+                np.zeros(self.layers[i]).\
+                    reshape(self.layers[i], 1)
+            )
+
+    def sigmoid(self, x):
         """
         calculates sigmoid function
         """
-
         return 1 / (1 + np.exp(-x))
 
-    def dSigmoid(self, a):
+    def dSigmoid(self, x):
+        sig = self.sigmoid(x)
+        return sig - (1 - sig)
+
+    def cost(self, x, y):
+        return np.mean((x - y) ** 2)
+
+    def dCost(self, x, y):
+        return np.mean(x - y)
+
+    def forward(self, x):
         """
-        calculates derivative of precalculated sigmoid function
+        forward propagation through network
         """
-        # sig = self.Sigmoid(x)
-        return a - (1.0 - a)
 
-    def MSE(self, x, y):
-        """
-        calculates mean squared error
-        """
-        assert x.size == y.size
+        self.params['as'][0] = x
 
-        return np.sum((x - y)**2) / x.size
+        for idx in np.arange(1, self.layers.size):
 
-    def dMSE(self, x, y):
-        """
-        implements partial derivative of MSE w.r.t. x
-        """
-        assert x.size == y.size
+            # index previous activations
+            a = self.params['as'][idx-1]
 
-        return (2 / x.size) * np.sum(x-y)
+            # index weights
+            w = self.params['weights'][idx-1]
 
-    def feedforward(self, x):
-        """
-        feeds input data through forward passes
-        implemented with dot matrix multiplication of
-        previous layer nodes and inter-layer weights
-        """
-        self.nodes = [x] + [np.zeros(x) for x in self.layers_config[1:]]
-        self.activations = [np.zeros_like(x) for x in self.nodes]
+            # index bias
+            b = self.params['bias'][idx-1]
 
-        for i in np.arange(self.layers_config.size-1):
-            self.nodes[i + 1] = \
-                (self.activations[i] @ self.weights[i]) + self.bias[i]
+            # calculate z_array
+            self.params['zs'][idx] = (
+                (a @ w.T) + b
+            )
 
-            self.activations[i+1] = self.Sigmoid(self.nodes[i+1])
+            # calculate activation
+            self.params['as'][idx] = self.sigmoid(
+                self.params['zs'][idx]
+            )
 
-    def backprop(self, y):
+        return self.params['as'][-1]
 
-        for ldx in np.arange(1, self.layers_config.size)[::-1]:
+    def backward(self, y):
 
-            if ldx == self.layers_config.size-1:
+        cache_dC_dA_dZ = []
+        d_weights = self.params['weights'].copy()
+        d_bias = self.params['bias'].copy()
 
-                # calculates initial loss
-                loss = self.MSE(self.activations[ldx], y)
+        for idx in np.arange(self.layers.size)[::-1]:
 
-                # deriv of Loss w.r.t Activation
-                dL_dA = self.dMSE(self.activations[ldx], y)
+            if idx == 0:
+                break
+
+            elif idx == self.layers.size - 1:
+
+                # derivative of cost wrt final activation
+                dC_dA = np.full(
+                    self.params['as'][idx].size,
+                    self.dCost(self.params['as'][idx], y)
+                )
 
             else:
 
-                # calculates loss from upstream neurons
-                loss = self.MSE(self.activations[ldx], self.activations[ldx+1])
-
-                # deriv of Loss w.r.t Activation
-                dL_dA = self.dMSE(self.activations[ldx], self.activations[ldx+1])
-
-            # deriv of Activation w.r.t Node
-            dA_dZ = self.dSigmoid(self.activations[ldx])
-
-            # deriv of Node w.r.t Weight
-            dZ_dW = self.activations[ldx-1]
-
-            # calculate partial deriviatives as product of previous terms
-            dL_dW = np.zeros_like(self.weights[ldx-1])
-            for a in np.arange(dA_dZ.size):
-                for w in np.arange(dZ_dW.size):
-                    dL_dW[w, a] = (
-                        dL_dA * dA_dZ[a] * dZ_dW[w]
-                    )
-
-            # update weights in layer
-            self.weights[ldx-1] = self.weights[ldx-1] - (self.learning_rate * dL_dW)
-
-            print(self.weights)
-
-    def fit(self):
-        pass
-
-    def predict(self):
-        pass
+                # calculates current activation derivative using cached layers
+                dC_dA = cache_dC_dA_dZ[-1] @ self.params['weights'][idx]
 
 
-def activation(x):
-    pass
+            # derivative of activation wrt z-layer
+            dA_dZ = self.dSigmoid(
+                self.params['as'][idx]
+            )
+
+
+            # derivative of z-layer wrt to weights
+            dZ_dW = self.params['as'][idx-1]
+
+            # perform shared multiplicative term
+            dC_dA_dZ = (dC_dA * dA_dZ)
+
+            # derivative of cost wrt to weights
+            dC_dW = self.learning_rate * (dC_dA_dZ.T @ dZ_dW)
+
+            # derivative of cost wrt to bias
+            dC_dB = (self.learning_rate * dC_dA_dZ).reshape(-1)
+
+
+            # cache calculated derivatives
+            cache_dC_dA_dZ.append(dC_dA_dZ)
+            d_weights[idx - 1] = dC_dW.copy()
+            d_bias[idx - 1] = dC_dB.copy()
+
+
+        self.d_weights.append(d_weights)
+        self.d_bias.append(d_bias)
+
+    def step(self):
+
+        # iterate through layers
+        for i in np.arange(len(self.params['weights'])):
+
+            # prepare tensor
+            d_weight_tensor = []
+            d_bias_tensor = []
+
+            # iterate through all derivatives
+            for mdx in np.arange(len(self.d_weights)):
+
+                dw = self.d_weights[mdx][i]
+                db = self.d_bias[mdx][i]
+
+                d_weight_tensor.append(dw)
+                d_bias_tensor.append(db)
+
+
+            # consolidate arrays into multidimensional tensor
+            d_weight_tensor = np.array(d_weight_tensor)
+            d_bias_tensor = np.array(d_bias_tensor)
+
+            # take mean across the zero axis
+            mean_d_weight_tensor = d_weight_tensor.mean(axis = 0)
+            mean_d_bias_tensor = d_bias_tensor.mean(axis = 0)
+
+            # update weights and biases
+            self.params['weights'][i] -= mean_d_weight_tensor
+            self.params['bias'][i] -= mean_d_bias_tensor
+
+    def clear(self):
+        self.d_weights = []
+        self.d_bias = []
+
+    def fit(self, x, y, n_epochs = 100, n_iter=50):
+
+        for e in np.arange(n_epochs):
+
+
+            pred = self.forward(x)
+            loss = self.cost(pred, y)
+
+            self.backward(y)
+
+            self.step()
+            self.clear()
+
+            if e % 10 == 0:
+                print("loss @ epoch {}: {:.4f}".format(e, loss))
 
 
 def main():
     np.random.seed(42)
 
+    n = 100
+    x = np.random.random((1, n))
+
     nn = NeuralNetwork(
-        layers=[8, 3, 8]
+        layers = [n, 5, 5, 5, n]
     )
-
-    input_data = np.random.random(8)
-    nn.feedforward(input_data)
-    nn.backprop(input_data)
-
+    nn.fit(x, x)
 
 if __name__ == '__main__':
     main()
